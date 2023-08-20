5e42cef8c695f3fc46ed4a0441cd2d3d3dcf2ac9
Rishabh Dev <devrishabh28@gmail.com>
2023-08-20 13:51:25 +0530

    modules in node

---
+++ basics/2-names.js
@@ -0,0 +1,6 @@
+const secret = 'SECRET';
+
+const light = 'light';
+const ayanokoji = 'ayanokoji';
+
+module.exports = {light, ayanokoji}
---
+++ basics/app.js
@@ -0,0 +1,6 @@
+const names = require('./2-names');
+const sayHi = require('./3-utils');
+
+sayHi('Goku');
+sayHi(names.light);
+sayHi(names.ayanokoji);
