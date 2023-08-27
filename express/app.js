22ed65704ee8faddadd78c66222245f29d6b7989
Rishabh Dev <devrishabh28@gmail.com>
2023-08-27 18:39:02 +0530

    express routers

--- express/app.js
+++ express/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-let { people } = require('./data')
-
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 app.use(express.static('./form-example'));
 
 //  parse form data.
@@ -9,61 +9,9 @@ app.use(express.urlencoded({ extended:false }))
 //  pasrse json
 app.use(express.json())
 
-app.get('/api/people', (req, res) => {
-	res.status(200).json({success: true, data: people})
-})
+app.use('/api/people', people);
+app.use('/login', auth);
 
-app.post('/api/people', (req, res) => {
-	const { name } = req.body
-
-	if(!name){
-		return res.status(400).json({success:false, msg:'please prorivde name values'})
-	}
-	res.status(201).json({success:true,person:name});
-})
-
-app.post('/login/', (req, res) => {
-	const { name } = req.body
-	if(name){
-		return res.status(200).send(`Welcome ${name}!`)
-	}
-
-	res.status(401).send('Please provide credentials.')
-})
-
-app.put('/api/people/:id', (req, res) => {
-	const {id} = req.params
-	const {name} = req.body
-
-	const person = people.find((person) => person.id === Number(id))
-
-	if(!person){
-		return res.status(404).json({success: false, msg:`no person with id ${id}`})
-	}
-
-	const newPeople = people.map((person) => {
-		if(person.id === Number(id)){
-			person.name = name;
-		}
-		return person;
-	})
-
-	return res.status(200).json({success: true, data: newPeople});
-})
-
-app.delete('/api/people/:id', (req, res) => {
-	const {id} = req.params
-
-	const person = people.find((person) => person.id === Number(id))
-
-	if(!person){
-		return res.status(404).json({success: false, msg:`no person with id ${id}`})
-	}
-
-	people = people.filter((person) => person.id !== Number(id))
-
-	return res.status(200).json({success: true, data: people});
-})
 app.listen(5000, () => {
 	console.log('server is listening on port 5000...')
 })
